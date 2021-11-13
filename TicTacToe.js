84df6a837625a28db75afb033f3f5dd3dc79e582
György Végh <gyorgy.vegh.job@gmail.com>
2021-11-13 17:05:27 +0100

    2Green

--- TicTacToe.js
+++ TicTacToe.js
@@ -1,5 +1,10 @@
 function guardians (P1, P2) {
-    return (typeof P1 == 'string' && typeof P2 == 'string')
+    if (P1 != '' && P2 != '')
+    {
+        return (typeof P1 == 'string' && typeof P2 == 'string');
+    } else {
+        return false;
+    }
 }
 
  function TicTacToe (P1, P2) {
--- TicTacToe.test.js
+++ TicTacToe.test.js
@@ -4,6 +4,6 @@ test('Are they strings?', () => {
   expect(guardians(12345,'Player1234')).toBe(false);
 });
 
-test('Are they strings?', () => {
+test('Are they not empty?', () => {
     expect(guardians('','')).toBe(false);
 });
