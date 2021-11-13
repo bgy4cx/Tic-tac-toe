58b14b4682f9bdc4d16fc53c3f15e35645629d9a
György Végh <gyorgy.vegh.job@gmail.com>
2021-11-13 16:53:57 +0100

    1ReFact

--- TicTacToe.js
+++ TicTacToe.js
@@ -1,10 +1,5 @@
 function guardians (P1, P2) {
-  if (typeof P1 == 'string' && typeof P2 == 'string') {
-    return true;
-  } else {
-    return false;  
-  }  
-  
+    return (typeof P1 == 'string' && typeof P2 == 'string')
 }
 
  function TicTacToe (P1, P2) {
